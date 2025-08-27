885e5b392cd198041dd2e66370552b82b1ad2cd5
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-08-27 23:12:16 +0700

    Initial Commit

---
+++ Rest Countries/Rest_CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rest_CountriesApp.swift
+//  Rest Countries
+//
+//  Created by Yaşar Çakır on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Rest_CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rest CountriesTests/Rest_CountriesTests.swift
@@ -0,0 +1,16 @@
+//
+//  Rest_CountriesTests.swift
+//  Rest CountriesTests
+//
+//  Created by Yaşar Çakır on 27.08.2025.
+//
+
+import Testing
+
+struct Rest_CountriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
